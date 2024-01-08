1cc725322b2ac563a2410d540202d5b3a7f310d2
Kevin Cordero <77799855+kevcorca@users.noreply.github.com>
2024-01-08 09:39:29 -0600

    Add files via upload

---
+++ CharAndBoolean/src/main/kotlin/Main.kt
@@ -0,0 +1,7 @@
+fun main(args: Array<String>) {
+    val myCharValue: Char = 'D'
+    val myBooleanValue: Boolean = false
+
+    println("$myCharValue")
+    println("My char value = $myCharValue and my boolean value = $myBooleanValue")
+}
---
+++ FloatAndDouble/src/main/kotlin/Main.kt
@@ -0,0 +1,7 @@
+fun main(args: Array<String>) {
+
+    val myNumber = 2f
+    val mySecondNumber = 2.0
+
+    println("float equals to $myNumber and double equals to $mySecondNumber")
+}
