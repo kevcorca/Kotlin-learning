c5e3aa86635d834b57ae61d9b4d25b07f61acbd9
Kevin Cordero <77799855+kevcorca@users.noreply.github.com>
2024-01-07 23:15:38 -0600

    Add files via upload

---
+++ ByteShortIntLong/src/main/kotlin/Main.kt
@@ -0,0 +1,28 @@
+fun main() {
+    var number = 22
+
+    val maxIntegerValue = Int.MAX_VALUE
+    val minIntegerValue = Int.MIN_VALUE
+    println("Int maximum value is $maxIntegerValue")
+    println("Int minimum value is $minIntegerValue")
+
+    number = 2147483647
+
+    val myMaxByteValue: Byte = Byte.MAX_VALUE
+    val myMinByteValue: Byte = Byte.MIN_VALUE
+    println("Byte maximum value is $myMaxByteValue")
+    println("Byte minimum value is $myMinByteValue")
+
+    val myMaxShortValue: Short = Short.MAX_VALUE
+    val myMinShortValue: Short = Short.MIN_VALUE
+    println("Short maximum value is $myMaxShortValue")
+    println("Short minimum value is $myMinShortValue")
+
+    val myMaxLongValue: Long = Long.MAX_VALUE
+    val myMinLongValue: Long = Long.MIN_VALUE
+    println("Long maximum value is $myMaxLongValue")
+    println("Long maximum value is $myMinLongValue ")
+
+    val myNumber = 28
+//    declare type para que no sea por default Int, tambien asi 28B, 28S, 28S, a veces automaticamente se hacen
+}
